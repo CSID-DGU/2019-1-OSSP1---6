d35b5ed98d268d7f77237ef1fe90c62cd858dc70
YoonhoRayLee <38045987+YoonhoRayLee@users.noreply.github.com>
2019-04-10 22:44:09 +0900

    Update deepLearning.py

--- deepLearning.py
+++ deepLearning.py
@@ -1,97 +1,252 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
+'''
+NumPy license
+Copyright © 2005-2019, NumPy Developers.
+All rights reserved.
+Redistribution and use in source and binary forms, with or without modification, are permitted provided 
+that the following conditions are met:
 
-This is a temporary script file.
-"""
-# 대표적인 비지도(Unsupervised) 학습 방법인 Autoencoder 를 구현해봅니다.
+Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer 
+in the documentation and/or other materials provided with the distribution.
+Neither the name of the NumPy Developers nor the names of any contributors may be used to endorse or promote products derived 
+from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, 
+BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
+OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, 
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+'''
+
+
+#%% Packages
 import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import os
+from tqdm import tqdm
 
-from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets("./mnist/data/", one_hot=True)
+#%% System Parameters
+# 1. Mini batch size
+mb_size = 128
+# 2. Missing rate
+p_miss = 0.5
+# 3. Hint rate
+p_hint = 0.9
+# 4. Loss Hyperparameters
+alpha = 10
+# 5. Imput Dim (Fixed)
+Dim = 784
+# 6. No
+Train_No = 55000
+Test_No = 10000
 
-#########
-# 옵션 설정
-######
-learning_rate = 0.01
-training_epoch = 20
-batch_size = 100
-# 신경망 레이어 구성 옵션
-n_hidden = 256  # 히든 레이어의 뉴런 갯수
-n_input = 28*28   # 입력값 크기 - 이미지 픽셀수
+#%% Data Input
+# MNIST
+mnist = input_data.read_data_sets('../../MNIST_data', one_hot = True)
 
-#########
-# 신경망 모델 구성
-######
-# Y 가 없습니다. 입력값을 Y로 사용하기 때문입니다.
-X = tf.placeholder(tf.float32, [None, n_input])
+# X
+trainX, _ = mnist.train.next_batch(Train_No) 
+testX, _  = mnist.test.next_batch(Test_No) 
 
-# 인코더 레이어와 디코더 레이어의 가중치와 편향 변수를 설정합니다.
-# 다음과 같이 이어지는 레이어를 구성하기 위한 값들 입니다.
-# input -> encode -> decode -> output
-W_encode = tf.Variable(tf.random_normal([n_input, n_hidden]))
-b_encode = tf.Variable(tf.random_normal([n_hidden]))
-# sigmoid 함수를 이용해 신경망 레이어를 구성합니다.
-# sigmoid(X * W + b)
-# 인코더 레이어 구성
-encoder = tf.nn.sigmoid(
-                tf.add(tf.matmul(X, W_encode), b_encode))
+# Mask Vector and Hint Vector Generation
+def sample_M(m, n, p):
+    A = np.random.uniform(0., 1., size = [m, n])
+    B = A > p
+    C = 1.*B
+    return C
+  
+trainM = sample_M(Train_No, Dim, p_miss)
+testM = sample_M(Test_No, Dim, p_miss)
 
-# encode 의 아웃풋 크기를 입력값보다 작은 크기로 만들어 정보를 압축하여 특성을 뽑아내고,
-# decode 의 출력을 입력값과 동일한 크기를 갖도록하여 입력과 똑같은 아웃풋을 만들어 내도록 합니다.
-# 히든 레이어의 구성과 특성치을 뽑아내는 알고리즘을 변경하여 다양한 오토인코더를 만들 수 있습니다.
-W_decode = tf.Variable(tf.random_normal([n_hidden, n_input]))
-b_decode = tf.Variable(tf.random_normal([n_input]))
-# 디코더 레이어 구성
-# 이 디코더가 최종 모델이 됩니다.
-decoder = tf.nn.sigmoid(
-                tf.add(tf.matmul(encoder, W_decode), b_decode))
+#%% Necessary Functions
+# 1. Xavier Initialization Definition
+def xavier_init(size):
+    in_dim = size[0]
+    xavier_stddev = 1. / tf.sqrt(in_dim / 2.)
+    return tf.random_normal(shape = size, stddev = xavier_stddev)
+    
+# 2. Plot (4 x 4 subfigures)
+def plot(samples):
+    fig = plt.figure(figsize = (5,5))
+    gs = gridspec.GridSpec(5,5)
+    gs.update(wspace=0.05, hspace=0.05)
+    
+    for i, sample in enumerate(samples):
+        ax = plt.subplot(gs[i])
+        plt.axis('off')
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_aspect('equal')
+        plt.imshow(sample.reshape(28,28), cmap='Greys_r')
+        
+    return fig
+   
+'''
+GAIN Consists of 3 Components
+- Generator
+- Discriminator
+- Hint Mechanism
+'''   
+   
+#%% GAIN Architecture   
+   
+#%% 1. Input Placeholders
+# 1.1. Data Vector
+X = tf.placeholder(tf.float32, shape = [None, Dim])
+# 1.2. Mask Vector 
+M = tf.placeholder(tf.float32, shape = [None, Dim])
+# 1.3. Hint vector
+H = tf.placeholder(tf.float32, shape = [None, Dim])
+# 1.4. Random Noise Vector
+Z = tf.placeholder(tf.float32, shape = [None, Dim])
 
-# 디코더는 인풋과 최대한 같은 결과를 내야 하므로, 디코딩한 결과를 평가하기 위해
-# 입력 값인 X 값을 평가를 위한 실측 결과 값으로하여 decoder 와의 차이를 손실값으로 설정합니다.
-cost = tf.reduce_mean(tf.pow(X - decoder, 2))
-optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
+#%% 2. Discriminator
+D_W1 = tf.Variable(xavier_init([Dim*2, 256]))     # Data + Hint as inputs
+D_b1 = tf.Variable(tf.zeros(shape = [256]))
 
-#########
-# 신경망 모델 학습
-######
-init = tf.global_variables_initializer()
+D_W2 = tf.Variable(xavier_init([256, 128]))
+D_b2 = tf.Variable(tf.zeros(shape = [128]))
+
+D_W3 = tf.Variable(xavier_init([128, Dim]))
+D_b3 = tf.Variable(tf.zeros(shape = [Dim]))       # Output is multi-variate
+
+theta_D = [D_W1, D_W2, D_W3, D_b1, D_b2, D_b3]
+
+#%% 3. Generator
+G_W1 = tf.Variable(xavier_init([Dim*2, 256]))     # Data + Mask as inputs (Random Noises are in Missing Components)
+G_b1 = tf.Variable(tf.zeros(shape = [256]))
+
+G_W2 = tf.Variable(xavier_init([256, 128]))
+G_b2 = tf.Variable(tf.zeros(shape = [128]))
+
+G_W3 = tf.Variable(xavier_init([128, Dim]))
+G_b3 = tf.Variable(tf.zeros(shape = [Dim]))
+
+theta_G = [G_W1, G_W2, G_W3, G_b1, G_b2, G_b3]
+
+#%% GAIN Function
+
+#%% 1. Generator
+def generator(x,z,m):
+    inp = m * x + (1-m) * z  # Fill in random noise on the missing values
+    inputs = tf.concat(axis = 1, values = [inp,m])  # Mask + Data Concatenate
+    G_h1 = tf.nn.relu(tf.matmul(inputs, G_W1) + G_b1)
+    G_h2 = tf.nn.relu(tf.matmul(G_h1, G_W2) + G_b2)
+    G_prob = tf.nn.sigmoid(tf.matmul(G_h2, G_W3) + G_b3) # [0,1] normalized Output
+    
+    return G_prob
+    
+#%% 2. Discriminator
+def discriminator(x, m, g, h):
+    inp = m * x + (1-m) * g  # Replace missing values to the imputed values
+    inputs = tf.concat(axis = 1, values = [inp,h])  # Hint + Data Concatenate
+    D_h1 = tf.nn.relu(tf.matmul(inputs, D_W1) + D_b1)
+    D_h2 = tf.nn.relu(tf.matmul(D_h1, D_W2) + D_b2)
+    D_logit = tf.matmul(D_h2, D_W3) + D_b3
+    D_prob = tf.nn.sigmoid(D_logit)  # [0,1] Probability Output
+    
+    return D_prob
+
+#%% 3. Others
+# Random sample generator for Z
+def sample_Z(m, n):
+    return np.random.uniform(0., 1., size = [m, n])        
+
+def sample_idx(m, n):
+    A = np.random.permutation(m)
+    idx = A[:n]
+    return idx
+
+#%% Structure
+G_sample = generator(X,Z,M)
+D_prob = discriminator(X, M, G_sample, H)
+
+#%% Loss
+D_loss1 = -tf.reduce_mean(M * tf.log(D_prob + 1e-8) + (1-M) * tf.log(1. - D_prob + 1e-8)) * 2
+G_loss1 = -tf.reduce_mean((1-M) * tf.log(D_prob + 1e-8)) / tf.reduce_mean(1-M)
+MSE_train_loss = tf.reduce_mean((M * X - M * G_sample)**2) / tf.reduce_mean(M)
+
+D_loss = D_loss1
+G_loss = G_loss1  + alpha * MSE_train_loss 
+
+#%% MSE Performance metric
+MSE_test_loss = tf.reduce_mean(((1-M) * X - (1-M)*G_sample)**2) / tf.reduce_mean(1-M)
+
+#%% Solver
+D_solver = tf.train.AdamOptimizer().minimize(D_loss, var_list=theta_D)
+G_solver = tf.train.AdamOptimizer().minimize(G_loss, var_list=theta_G)
+
+# Sessions
 sess = tf.Session()
-sess.run(init)
+sess.run(tf.global_variables_initializer())
 
-total_batch = int(mnist.train.num_examples/batch_size)
+#%%
+# Output Initialization
+if not os.path.exists('Multiple_Impute_out1/'):
+    os.makedirs('Multiple_Impute_out1/')
+    
+# Iteration Initialization
+i = 1
 
-for epoch in range(training_epoch):
-    total_cost = 0
-
-    for i in range(total_batch):
-        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
-        _, cost_val = sess.run([optimizer, cost],
-                               feed_dict={X: batch_xs})
-        total_cost += cost_val
-
-    print('Epoch:', '%04d' % (epoch + 1),
-          'Avg. cost =', '{:.4f}'.format(total_cost / total_batch))
-
-print('최적화 완료!')
-
-#########
-# 결과 확인
-# 입력값(위쪽)과 모델이 생성한 값(아래쪽)을 시각적으로 비교해봅니다.
-######
-sample_size = 10
-
-samples = sess.run(decoder,
-                   feed_dict={X: mnist.test.images[:sample_size]})
-
-fig, ax = plt.subplots(2, sample_size, figsize=(sample_size, 2))
-
-for i in range(sample_size):
-    ax[0][i].set_axis_off()
-    ax[1][i].set_axis_off()
-    ax[0][i].imshow(np.reshape(mnist.test.images[i], (28, 28)))
-    ax[1][i].imshow(np.reshape(samples[i], (28, 28)))
-
-plt.show()
+#%% Start Iterations
+for it in tqdm(range(10000)):    
+    
+    #%% Inputs
+    mb_idx = sample_idx(Train_No, mb_size)
+    X_mb = trainX[mb_idx,:]  
+    Z_mb = sample_Z(mb_size, Dim) 
+    M_mb = trainM[mb_idx,:]  
+    H_mb1 = sample_M(mb_size, Dim, 1-p_hint)
+    H_mb = M_mb * H_mb1
+    
+    New_X_mb = M_mb * X_mb + (1-M_mb) * Z_mb  # Missing Data Introduce
+    
+    _, D_loss_curr = sess.run([D_solver, D_loss1], feed_dict = {X: X_mb, M: M_mb, Z: New_X_mb, H: H_mb})
+    _, G_loss_curr, MSE_train_loss_curr, MSE_test_loss_curr = sess.run([G_solver, G_loss1, MSE_train_loss, MSE_test_loss],
+                                                                       feed_dict = {X: X_mb, M: M_mb, Z: New_X_mb, H: H_mb})
+            
+    #%% Output figure
+    if it % 100 == 0:
+      
+        mb_idx = sample_idx(Test_No, 5)
+        X_mb = testX[mb_idx,:]
+        M_mb = testM[mb_idx,:]  
+        Z_mb = sample_Z(5, Dim) 
+    
+        New_X_mb = M_mb * X_mb + (1-M_mb) * Z_mb
+        
+        samples1 = X_mb                
+        samples5 = M_mb * X_mb + (1-M_mb) * Z_mb
+        
+        samples2 = sess.run(G_sample, feed_dict = {X: X_mb, M: M_mb, Z: New_X_mb})
+        samples2 = M_mb * X_mb + (1-M_mb) * samples2        
+        
+        Z_mb = sample_Z(5, Dim) 
+        New_X_mb = M_mb * X_mb + (1-M_mb) * Z_mb       
+        samples3 = sess.run(G_sample, feed_dict = {X: X_mb, M: M_mb, Z: New_X_mb})
+        samples3 = M_mb * X_mb + (1-M_mb) * samples3     
+        
+        Z_mb = sample_Z(5, Dim) 
+        New_X_mb = M_mb * X_mb + (1-M_mb) * Z_mb       
+        samples4 = sess.run(G_sample, feed_dict = {X: X_mb, M: M_mb, Z: New_X_mb})
+        samples4 = M_mb * X_mb + (1-M_mb) * samples4     
+        
+        samples = np.vstack([samples5, samples2, samples3, samples4, samples1])          
+        
+        fig = plot(samples)
+        plt.savefig('Multiple_Impute_out1/{}.png'.format(str(i).zfill(3)), bbox_inches='tight')
+        i += 1
+        plt.close(fig)
+        
+    #%% Intermediate Losses
+    if it % 100 == 0:
+        print('Iter: {}'.format(it))
+        print('Train_loss: {:.4}'.format(MSE_train_loss_curr))
+        print('Test_loss: {:.4}'.format(MSE_test_loss_curr))
+        print()
+    if it == 10000:
+        print('Training Done!')
